9c085dacc178987bb2f1cb7937cf7d498061e8cf
AlexDeFoc <alex.de.foc@gmail.com>
2024-09-22 13:47:55 +0300

    day 1 vulkan

---
+++ 009-vulkan/c2_dev_env/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Paths
+bin="bin/"
+exe="$bin/main.exe"
+
+# Third-party headers
+H_glm="C:/App/Libraries"
+H_glfw="C:/App/Libraries/glfw/include"
+H_vulkan="C:/App/Libraries/VulkanSDK/1.3.290.0/Include"
+
+# Third-party libs
+L_glfw="C:/App/Libraries/glfw/lib-static-ucrt"
+l_glfw="glfw3dll"
+
+L_vulkan="C:/App/Libraries/VulkanSDK/1.3.290.0/Lib"
+l_vulkan="vulkan-1"
+
+# Source files
+main="src/main.cpp"
+
+# Create bin directory if it doesn't exist
+mkdir -p $bin
+
+# Compile & link command (compiling all source files into one executable)
+clang++ -o $exe $main -I $H_glm -I $H_glfw -I $H_vulkan -L $L_glfw -l $l_glfw -L $L_vulkan -l $l_vulkan
